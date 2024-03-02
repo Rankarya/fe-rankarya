2495348b23d89d3bacf92371278152874b94d256
24aeronyx <11211017@student.itk.ac.id>
2024-03-02 20:31:18 +0800

    Tailwind init

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 
 function App() {
   return (
-    <>
+    <div>
+      <ul className='flex gap-4'>
+        <li>tes1</li>
+        <li>tes2</li>
+      </ul>
+    </div>
   
-    </>
+    
   )
 }
 
