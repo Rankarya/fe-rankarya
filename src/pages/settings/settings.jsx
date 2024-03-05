512df476f41a2aed3080fcb744551e5639de0a8c
24aeronyx <11211017@student.itk.ac.id>
2024-03-05 21:41:12 +0800

    Create branch settings

---
+++ src/pages/settings/settings.jsx
@@ -0,0 +1,24 @@
+export default function Settings() {
+    return (
+      <div>
+        <div>
+          <ul>
+            <li>Profile</li>
+            <li>Account</li>
+            <li>Theme</li>
+            <li>Subscriptions</li>
+            <li>Wallet</li>
+          </ul>
+          <ul>
+            <li>Help Center</li>
+            <li>Terms of Use</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+  
