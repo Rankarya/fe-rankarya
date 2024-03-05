2cb6ea464ff006856ee2f772a5acc23d08a049ff
naufyl28 <11211060@student.itk.ac.id>
2024-03-05 18:12:24 +0800

    landingpage dikit lagi sm benerin footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+import LandingPage from "./pages/Landingpage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<LandingPage />} />
+    </>
+  )
+);
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <RouterProvider router={router} />
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
