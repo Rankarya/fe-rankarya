739456473a327aad37debd23b2ffcee9e23c659d
Naflah Shafa <11211066@student.itk.ac.id>
2024-03-06 00:09:48 +0800

    postingan & search

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ListPostingan from "./pages/ListPostingan.jsx";
+
 
 function App() {
   return (
-    <div>
-      <ul className='flex gap-4'>
-        <li>tes1</li>
-        <li>tes2</li>
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+      <Route path={"/post"} element={<ListPostingan />} />
+      </Routes>
+    </Router>
+    // <div>
+    //   <ul className='flex gap-4'>
+    //     <li>tes1</li>
+    //     <li>tes2</li>
+    //   </ul>
+    // </div>
   
     
   )
---
+++ src/pages/ListPostingan.jsx
@@ -0,0 +1,13 @@
+import Postingan from "../components/Postingan.jsx";
+import Search from "../components/Search.jsx";
+
+const ListPostingan = () => {
+  return (
+    <>
+        <Search/>
+        <Postingan/>
+    </>
+  )
+}
+
+export default ListPostingan;
