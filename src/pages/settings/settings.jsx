bd6bd1f28314fba199d744c4336d73ccbbfab504
24aeronyx <11211017@student.itk.ac.id>
2024-03-06 01:11:49 +0800

    Create account settings and theme settings page

--- src/pages/settings/settings.jsx
+++ src/pages/settings/settings.jsx
@@ -1,12 +1,19 @@
 import Sidebar from "../../components/sidebar";
+import Account from "./account";
 import Profile from "./profile";
+import Theme from "./theme";
+import Updateprofile from "./updateprofile";
 
 export default function Settings() {
   return (
     <div className="flex">
       <Sidebar />
-      <div>
+      <div className="border-l-2 pl-8 pt-2">
+        <div className="font-bold mb-4 text-xl">Settings</div>
         <Profile />
+        {/* <Updateprofile/> */}
+        {/* <Account/> */}
+        <Theme/>
       </div>
     </div>
   );
